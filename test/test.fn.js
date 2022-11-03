f7dbbec2a8a52b4e6b8fb6ce476894c55e1a8991
phuctv302 <phuc.tv194354@sis.hust.edu.vn>
2022-11-03 11:40:42 +0700

    minor fix

--- test/test.fn.js
+++ test/test.fn.js
@@ -2,3 +2,7 @@
  * @brief just for testing anything you want, and not impact to this app
  * @notice should delete all code after finishing testing, otherwise app will be crashed if there has any error in here ^ ^
  */
+
+const AppUtils = require('../utils/app.utils')
+
+AppUtils.writeErrorLog('Test error writing')
